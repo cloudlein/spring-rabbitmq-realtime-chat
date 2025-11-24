5ff454ad5c35b96d845955692733f29488ce0e6a
Salahudin-cloud <ayubi54321@gmail.com>
2025-11-24 13:01:58 +0700

    feat: [WIP] implement security role access for api

--- src/main/java/com/demo/chatApp/domain/service/impl/UserServiceImpl.java
+++ src/main/java/com/demo/chatApp/domain/service/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import com.demo.chatApp.domain.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,28 +17,33 @@ public class UserServiceImpl extends BaseService implements UserService {
 
     private final UserRepository userRepository;
 
+    @Secured("ROLE_ADMIN")
     @Transactional
     @Override
     public void create(User user) {
         userRepository.save(user);
     }
 
+    @Secured({"ROLE_ADMIN", "ROLE_USER"})
     @Override
     public User getById(Long id) {
         return orNotFound(userRepository.findById(id), "User not found");
     }
 
+    @Secured({"ROLE_ADMIN", "ROLE_USER"})
     @Transactional
     @Override
     public void update(User user) {
         userRepository.save(user);
     }
 
+    @Secured("ROLE_ADMIN")
     @Override
     public Page<User> findAll(int page, int size) {
         return userRepository.findAll(PageRequest.of(page, size));
     }
 
+    @Secured("ROLE_ADMIN")
     @Transactional
     @Override
     public void delete(Long id) {
@@ -45,11 +51,13 @@ public class UserServiceImpl extends BaseService implements UserService {
         userRepository.deleteUser(existing);
     }
 
+    @Secured({"ROLE_ADMIN", "ROLE_USER"})
     @Override
     public User getByUsername(String username) {
         return orNotFound(userRepository.findByUsername(username), "Username not found");
     }
 
+    @Secured({"ROLE_ADMIN", "ROLE_USER"})
     @Override
     public boolean existByUsername(String username) {
         return userRepository.checkUserExists(username);
